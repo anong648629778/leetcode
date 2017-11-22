ecc84c8198e08f7deb2961e59f97a9c58c803720
huanghaidong <haidong.huang@szzjcx.com>
2017-11-22 17:17:48 +0800

    添加解题思路

--- src/leetcode/ArrayPartitionI.java
+++ src/leetcode/ArrayPartitionI.java
@@ -2,6 +2,12 @@ package leetcode;
 
 import java.util.Arrays;
 
+/**
+ * 题目大意：有长度为2n的数组，怎么样组合(a1,b1),(a2,b2)...(ai,bi)才能让min(a1,b1) + min(a2, b2) +...+min(ai,bi)的和最大
+ * 解题思路:让小的数，跟大于它的最小数组合，就是正解
+ * @author haidong.huang
+ *
+ */
 public class ArrayPartitionI {
 	
 	public static int arrayPairSum(int[] nums) {

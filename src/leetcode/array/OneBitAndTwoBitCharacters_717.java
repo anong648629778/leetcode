e506d2bd12ceec8088293ce83d8766541b94bcbe
huanghaidong <haidong.huang@szzjcx.com>
2017-11-23 16:41:25 +0800

    test

---
+++ src/leetcode/array/OneBitAndTwoBitCharacters_717.java
@@ -0,0 +1,18 @@
+package leetcode.array;
+
+/**
+ * 题目大意：大概就是给一个由0，1组成的数据，这些数字有两种组成方式，一种是单单由0组成，一种是有10 或者11这样的形式组成，问是不是最后一种组成一定会是0
+ * 解题思路：
+ * @author huanghaidong
+ *
+ */
+public class OneBitAndTwoBitCharacters_717 {
+	
+	public static boolean isOneBitCharacter(int[] bits) {
+		return false;
+    }
+	
+	public static void main(String []args) {
+		System.out.println(isOneBitCharacter(new int[]{1, 1, 1, 0}));
+	}
+}

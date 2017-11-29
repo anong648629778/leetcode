e09eac428a0802d09fa77e108a063bb1ab0720fe
Administrator <Administrator@USER-20170909CC>
2017-11-29 21:08:34 +0800

    ContainsDuplicateII_219

---
+++ src/leetcode/array/ContainsDuplicateII_219.java
@@ -0,0 +1,23 @@
+package leetcode.array;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ContainsDuplicateII_219 {
+	
+	public boolean containsNearbyDuplicate(int[] nums, int k) {
+		Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0 ;i < nums.length; i++) {
+        	if (map.containsKey(nums[i]) &&  i - map.get(nums[i]) <= k) {
+        		return true;
+        	}
+        	map.put(nums[i], i);
+        }
+        return false;
+    }
+
+	public static void main(String[] args) {
+
+	}
+
+}

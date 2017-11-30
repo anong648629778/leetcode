b415b210044e49ffc60361e8576e36074b8f6e3e
huanghaidong <haidong.huang@szzjcx.com>
2017-11-30 10:16:29 +0800

    MajorityElement_169

--- src/leetcode/array/MajorityElement_169.java
+++ src/leetcode/array/MajorityElement_169.java
@@ -1,8 +1,23 @@
 package leetcode.array;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class MajorityElement_169 {
 	
 	public int majorityElement(int[] nums) {
+	    Map<Integer, AtomicInteger> map = new HashMap<>();
+	    for (int i = 0; i < nums.length; i++) {
+	        if (null == map.get(nums[i])) {
+	            map.put(nums[i], new AtomicInteger(1));
+	        } else {
+	            map.get(nums[i]).incrementAndGet();
+	        }
+	        if (map.get(nums[i]).get() > nums.length / 2) {
+	            return nums[i];
+	        }
+	    }
         return 0;
     }
 

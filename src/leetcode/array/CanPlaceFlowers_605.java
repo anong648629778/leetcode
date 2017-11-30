497ad7a036d82ffe90b6c5e4e534ebc7f77f5cac
huanghaidong <haidong.huang@szzjcx.com>
2017-11-30 18:31:50 +0800

    CanPlaceFlowers_605

---
+++ src/leetcode/array/CanPlaceFlowers_605.java
@@ -0,0 +1,25 @@
+package leetcode.array;
+
+public class CanPlaceFlowers_605 {
+
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int cnt = 0;
+        for (int i = 0; i < flowerbed.length; i++) {
+            if (flowerbed[i] == 0) {
+                if (i == 0 && flowerbed.length == 1 
+                        || i == 0 && i + 1 < flowerbed.length && flowerbed[i + 1] == 0
+                        || i == flowerbed.length - 1 && i - 1 >= 0 && flowerbed[i - 1] == 0
+                        || i - 1 >= 0 && i + 1 < flowerbed.length && flowerbed[i - 1] == 0 && flowerbed[i + 1] == 0) {
+                    flowerbed[i] = 1;
+                    cnt++;
+                }
+            }
+        }
+        return n <= cnt;
+    }
+
+    public static void main(String[] args) {
+        new CanPlaceFlowers_605().canPlaceFlowers(new int[] {0, 1, 0}, 1);
+    }
+
+}

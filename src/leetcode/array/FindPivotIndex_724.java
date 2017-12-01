03335341295c3ecafe369e761f009dad47217cf1
huanghaidong <haidong.huang@szzjcx.com>
2017-12-01 09:23:30 +0800

    FindPivotIndex_724

--- src/leetcode/array/FindPivotIndex_724.java
+++ src/leetcode/array/FindPivotIndex_724.java
@@ -5,18 +5,21 @@ public class FindPivotIndex_724 {
     public int pivotIndex(int[] nums) {
         int rightSum = 0;
         int leftSum = 0;
-        for (int i = 0;i < nums.length; i++) {
+        for (int i = 0; i < nums.length; i++) {
             rightSum += nums[i];
         }
         for (int i = 0; i < nums.length; i++) {
-            
+            rightSum -= nums[i];
+            if (leftSum == rightSum) {
+                return i;
+            }
+            leftSum += nums[i];
         }
         return -1;
     }
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-
+        System.out.println(new FindPivotIndex_724().pivotIndex(new int[] {1,7,3,6,5,6}));
     }
 
 }
